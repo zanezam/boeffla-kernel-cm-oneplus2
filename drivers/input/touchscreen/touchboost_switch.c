f875bcbf34c98c48f3661d700c8d8fc9c95ee1e6
andip71 <andreasp@gmx.de>
2017-09-01 18:49:56 +0200

    Revert "cpu-boost: Update touch boost (input boost) configuration driver to V2"
    This reverts commit 9785fd25072b760eb6e2132d8fe0676795c6d8f7.

--- drivers/input/touchscreen/touchboost_switch.c
+++ drivers/input/touchscreen/touchboost_switch.c
@@ -1,7 +1,7 @@
 /*
- * interface to control touch boost (= input boost) on OnePlus Two - V2
+ * interface to control touch boost (= input boost) on OnePlus One bacon
  *
- * Author: andip71, 20.01.2016
+ * Author: andip71, 22.09.2014
  *
  *
  * This software is licensed under the terms of the GNU General Public
@@ -21,36 +21,27 @@
 #include <linux/stat.h>
 
 
-#define TOUCHBOOST_DURATION_MIN		0
-#define TOUCHBOOST_DURATION_MAX		10000
-
-
 /*****************************************/
 // Global external variables
 /*****************************************/
 
-extern unsigned int touchboost_status_1;
-extern unsigned int touchboost_freq_1;
-extern unsigned int touchboost_status_2;
-extern unsigned int touchboost_freq_2;
-extern unsigned int touchboost_ms;
+extern unsigned int input_boost_status;
+extern unsigned int input_boost_freq;
 
-void set_touchboost_parameters(void);
-void get_touchboost_parameters(void);
-
+void set_touchboost_frequency(void);
 
 /*****************************************/
 // sysfs interface functions
 /*****************************************/
 
-static ssize_t touchboost_switch_show_1(struct device *dev, struct device_attribute *attr, char *buf)
+static ssize_t touchboost_switch_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	get_touchboost_parameters();
-	return sprintf(buf, "Touchboost status L: %d", touchboost_status_1);
+	// return value of current touchboost status
+	return sprintf(buf, "Touchboost status: %d", input_boost_status);
 }
 
 
-static ssize_t touchboost_switch_store_1(struct device *dev, struct device_attribute *attr,
+static ssize_t touchboost_switch_store(struct device *dev, struct device_attribute *attr,
 					const char *buf, size_t count)
 {
 	unsigned int ret = -EINVAL;
@@ -62,57 +53,23 @@ static ssize_t touchboost_switch_store_1(struct device *dev, struct device_attri
 	if (ret != 1)
 		return -EINVAL;
 
-	// check if new status is valid
-	if ((val != 0) && (val != 1))
-	{
-		pr_err("Touchboost switch: invalid touchboost L status.\n");
-		return -EINVAL;
-	}
+	// check if new status is valid and store in external variable
+	if ((val == 0) || (val == 1))
+		input_boost_status = val;
+	else
+		pr_err("Touchboost switch : invalid touchboost status.\n");
 
-	touchboost_status_1 = val;
-	set_touchboost_parameters();
-	return count;
-
-}
-
-static ssize_t touchboost_switch_show_2(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	get_touchboost_parameters();
-	return sprintf(buf, "Touchboost status B: %d", touchboost_status_2);
-}
-
-
-static ssize_t touchboost_switch_store_2(struct device *dev, struct device_attribute *attr,
-					const char *buf, size_t count)
-{
-	unsigned int ret = -EINVAL;
-	unsigned int val = 0;
-
-	// read value from input buffer
-	ret = sscanf(buf, "%d", &val);
-
-	if (ret != 1)
-		return -EINVAL;
-
-	// check if new status is valid
-	if ((val != 0) && (val != 1))
-	{
-		pr_err("Touchboost switch: invalid touchboost B status.\n");
-		return -EINVAL;
-	}
-
-	touchboost_status_2 = val;
-	set_touchboost_parameters();
 	return count;
 }
 
-static ssize_t touchboost_freq_show_1(struct device *dev, struct device_attribute *attr, char *buf)
+static ssize_t touchboost_freq_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	get_touchboost_parameters();
-	return sprintf(buf, "%d - Touchboost frequency\n", touchboost_freq_1);
+	// return value of current touchboost frequency
+	return sprintf(buf, "%d - Touchboost frequency\n", input_boost_freq);
 }
 
-static ssize_t touchboost_freq_store_1(struct device *dev, struct device_attribute *attr,
+
+static ssize_t touchboost_freq_store(struct device *dev, struct device_attribute *attr,
 					const char *buf, size_t count)
 {
 	unsigned int ret = -EINVAL;
@@ -125,70 +82,18 @@ static ssize_t touchboost_freq_store_1(struct device *dev, struct device_attribu
 		return -EINVAL;
 
 	if (input == 0) 
-		return -EINVAL;
-
-	touchboost_freq_1 = input;
-	set_touchboost_parameters();
-	return count;
-}
-
-static ssize_t touchboost_freq_show_2(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	get_touchboost_parameters();
-	return sprintf(buf, "%d - Touchboost frequency\n", touchboost_freq_2);
-}
-
-
-static ssize_t touchboost_freq_store_2(struct device *dev, struct device_attribute *attr,
-					const char *buf, size_t count)
-{
-	unsigned int ret = -EINVAL;
-	unsigned int input = 0;
-
-	// read value from input buffer
-	ret = sscanf(buf, "%d", &input);
-
-	if (ret != 1)
-		return -EINVAL;
-
-	if (input == 0) 
-		return -EINVAL;
-
-	touchboost_freq_2 = input;
-	set_touchboost_parameters();
-	return count;
-}
-
-
-static ssize_t touchboost_ms_show(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	get_touchboost_parameters();
-	return sprintf(buf, "%d - Touchboost impulse length (ms)\n", touchboost_ms);
-}
-
-
-static ssize_t touchboost_ms_store(struct device *dev, struct device_attribute *attr,
-					const char *buf, size_t count)
-{
-	unsigned int ret = -EINVAL;
-	unsigned int input = 0;
-
-	// read value from input buffer
-	ret = sscanf(buf, "%d", &input);
-
-	if (ret != 1)
-		return -EINVAL;
-
-	// check if value is valid
-	if ((input < TOUCHBOOST_DURATION_MIN) || (input > TOUCHBOOST_DURATION_MAX))
 	{
-		pr_err("Touchboost switch: invalid duration value requested");
+		pr_err("Touchboost switch : frequency not allowed");
 		return -EINVAL;
+	} 
+	else 
+	{
+		input_boost_freq = input;
+		set_touchboost_frequency();
+		pr_debug("Touchboost switch : frequency for touch boost set");
 	}
 
-	touchboost_ms = input;
-	set_touchboost_parameters();
-	return count;
+	return -EINVAL;
 }
 
 
@@ -197,19 +102,13 @@ static ssize_t touchboost_ms_store(struct device *dev, struct device_attribute *
 /*****************************************/
 
 // define objects
-static DEVICE_ATTR(touchboost_switch, S_IRUGO | S_IWUGO, touchboost_switch_show_1, touchboost_switch_store_1);
-static DEVICE_ATTR(touchboost_freq  , S_IRUGO | S_IWUGO, touchboost_freq_show_1 ,touchboost_freq_store_1);
-static DEVICE_ATTR(touchboost_switch_2, S_IRUGO | S_IWUGO, touchboost_switch_show_2, touchboost_switch_store_2);
-static DEVICE_ATTR(touchboost_freq_2  , S_IRUGO | S_IWUGO, touchboost_freq_show_2 , touchboost_freq_store_2);
-static DEVICE_ATTR(touchboost_ms  , S_IRUGO | S_IWUGO, touchboost_ms_show , touchboost_ms_store);
+static DEVICE_ATTR(touchboost_switch, S_IRUGO | S_IWUGO, touchboost_switch_show, touchboost_switch_store);
+static DEVICE_ATTR(touchboost_freq  , S_IRUGO | S_IWUGO, touchboost_freq_show  , touchboost_freq_store  );
 
 // define attributes
 static struct attribute *touchboost_switch_attributes[] = {
 	&dev_attr_touchboost_switch.attr,
 	&dev_attr_touchboost_freq.attr,
-	&dev_attr_touchboost_switch_2.attr,
-	&dev_attr_touchboost_freq_2.attr,
-	&dev_attr_touchboost_ms.attr,
 	NULL
 };
 
@@ -235,7 +134,7 @@ static int touchboost_switch_init(void)
 	misc_register(&touchboost_switch_control_device);
 	if (sysfs_create_group(&touchboost_switch_control_device.this_device->kobj,
 				&touchboost_switch_control_group) < 0) {
-		pr_info("Touchboost switch : failed to create touchboost switch sys fs object.\n");
+		pr_err("Touchboost switch : failed to create touchboost switch sys fs object.\n");
 		return 0;
 	}
 
